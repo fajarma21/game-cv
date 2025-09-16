981055191e3bcc4de51d5ec18fa8489c87ff0373
Fajar Maulana <fajarma21@gmail.com>
2025-09-16 15:22:17 +0700

    feat(name): change name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fajarma-game',
+  base: '/game-cv',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
